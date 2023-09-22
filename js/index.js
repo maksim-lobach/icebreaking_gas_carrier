2f6604bd9676d136ebaba12ff6168784a147a82a
maksim-lobach <145792647+maksim-lobach@users.noreply.github.com>
2023-09-23 01:18:55 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
+var list = document.getElementById("listUlMenu");
+var buttonClick = document.getElementById("burgerButton");
+
+let widchScreen = 750;
+
+buttonClick.addEventListener("click", () => {
+  list.classList.toggle("activeMenu");
+  buttonClick.classList.toggle("activeButton");
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > widchScreen) {
+    buttonClick.classList.remove("activeButton");
+    list.classList.remove("activeMenu");
+  }
+});
+
 
